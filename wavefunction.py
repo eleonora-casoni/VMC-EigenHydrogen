96ddeca54079319b8f24040c0eadedbabe3a559c
Eleonora <eleonora.casoni2@studio.unibo.it>
2025-02-06 12:53:56 +0100

    added wavefunction script

---
+++ wavefunction.py
@@ -0,0 +1,19 @@
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+
+
+
+def trial_wavefunction(x, alpha):
+    """Computes the trial wavefunction for given positions x and parameter alpha."""
+    if isinstance(x, np.ndarray):
+        return np.exp(-alpha * x) * (x > 0)  # Element-wise multiplication applies the condition
+    return np.exp(-alpha * x) if x > 0 else 0
+
+def local_energy_func(x, alpha):
+    """Computes the local energy for given positions x and parameter alpha."""
+    if np.any(x == 0):  # Avoid division by zero
+        raise ValueError("Position x must be nonzero to avoid division by zero.")
+    
+    return (-1 + alpha * x - (alpha**2) * x / 2) / x
