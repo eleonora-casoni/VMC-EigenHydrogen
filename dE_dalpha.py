650c52264c18bed9ad4544827e6974f5752fcd4a
Eleonora <eleonora.casoni2@studio.unibo.it>
2025-02-16 16:15:31 +0100

    removed unnecessary files

--- dE_dalpha.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from local_energy import local_energy_func
-
-def dE_dalpha(x, alpha):
-    El = local_energy_func(x, alpha)
-    ln_wf = -x  # d(log Ψ)/dα
-    return 2 * (np.mean(El * ln_wf) - np.mean(El) * np.mean(ln_wf))
