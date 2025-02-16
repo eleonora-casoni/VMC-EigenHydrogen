d81b4352e56140fe96627ff4053434e639ebb026
Eleonora <eleonora.casoni2@studio.unibo.it>
2025-02-16 16:10:10 +0100

    moved all the functions of the algorithm together

---
+++ simulation.py
@@ -0,0 +1,53 @@
+import numpy as np 
+from tqdm import tqdm
+
+def trial_wavefunction(x, alpha):
+    """Computes the trial wavefunction for given positions x and parameter alpha."""
+    result = np.exp(-alpha * x)
+    if np.any(x <= 0): 
+     raise ValueError("Negative and 0 values for the distance are not allowed for this physical system.")
+    
+    return result 
+
+def local_energy_func(x, alpha):
+    """Computes the local energy for given positions x and parameter alpha."""
+    if np.any(x == 0):  # Avoid division by zero
+        raise ValueError("Position x must be nonzero to avoid division by zero.")
+    
+    return (-1 + alpha * x - (alpha**2) * x / 2) / x
+
+def dE_dalpha(x, alpha):
+    El = local_energy_func(x, alpha)
+    ln_wf = -x  # d(log Ψ)/dα
+    return 2 * (np.mean(El * ln_wf) - np.mean(El) * np.mean(ln_wf))
+
+def alpha_opt_on_fly(position_vec, alpha):
+    dE_da = dE_dalpha(position_vec, alpha)
+    alpha = alpha - 0.01 * dE_da  # Learning rate = 0.01
+    return alpha, dE_da
+
+
+def metropolis(equilibration_steps, numsteps, numwalkers, alpha):
+    position_vec = np.random.uniform(low=2, high=3, size=numwalkers)  # Initialize positions
+    initial_pos = position_vec
+    alpha_buffer = np.empty(numsteps)
+    dE_da_buffer = np.empty(numsteps)
+    E_buffer = np.empty(numsteps)
+
+    for j in tqdm(range(numsteps)):  # Progress bar
+        for i in range(equilibration_steps):
+            new_position_vec = position_vec + 0.1*np.random.randn(numwalkers)
+            # Reject moves where x < 0 (those walkers stay at their previous positions)
+            valid_moves = new_position_vec > 0
+            new_position_vec = np.where(valid_moves, new_position_vec, position_vec)
+            p = trial_wavefunction(new_position_vec, alpha) / trial_wavefunction(position_vec, alpha)
+            rand_unif_array = np.random.uniform(size=len(p))
+            position_vec = np.where(p > rand_unif_array, new_position_vec, position_vec)
+
+        # Optimize α
+        alpha, dE_da = alpha_opt_on_fly(position_vec, alpha)
+        alpha_buffer[j] = alpha
+        dE_da_buffer[j] = dE_da
+        E_buffer[j] = np.mean(local_energy_func(position_vec, alpha))
+
+    return position_vec, alpha, alpha_buffer, E_buffer, dE_da_buffer, initial_pos
