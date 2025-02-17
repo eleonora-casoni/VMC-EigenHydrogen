8a9af055e8979bef9ff05a699cb9be46cd399ad2
Eleonora <eleonora.casoni2@studio.unibo.it>
2025-02-17 17:52:05 +0100

    corrected local energy calculation

--- simulation.py
+++ simulation.py
@@ -12,7 +12,7 @@ def local_energy_func(x, alpha):
     if np.any(x == 0):  # Avoid division by zero
         raise ValueError("Position x must be nonzero to avoid division by zero.")
     
-    return (-1 + alpha * x - (alpha**2) * x / 2) / x
+    return -1/x - (alpha**2)/2 + alpha/x
 
 def dE_dalpha(x, alpha):
     El = local_energy_func(x, alpha)
