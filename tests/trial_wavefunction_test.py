dc40706d507417479e706c32c55a9193e076a6ff
Eleonora <eleonora.casoni2@studio.unibo.it>
2025-02-17 12:23:44 +0100

    more testing for position values

--- tests/trial_wavefunction_test.py
+++ tests/trial_wavefunction_test.py
@@ -3,7 +3,7 @@ import pytest
 from simulation import trial_wavefunction
 
 
-def test_trial_wavefunction_negative_positions():
+def test_negative_positions():
     """
     Test that trial_wavefunction() returns 0 for negative positions.
 
@@ -19,7 +19,7 @@ def test_trial_wavefunction_negative_positions():
 
     assert np.array_equal(result, expected_output), "The function did not return 0 for negative positions."
 
-def test_trial_wavefunction_zero_position():
+def test_zero_position():
     """
     Test that trial_wavefunction() returns 0 for position = 0.
 
@@ -34,7 +34,7 @@ def test_trial_wavefunction_zero_position():
 
     assert np.array_equal(result, expected_output), "The function did not return 0 for position = 0."
 
-def test_trial_wavefunction_large_positive_x():
+def test_large_positive_x():
     """
     Test that the trial_wavefunction returns a value close to 0 for large positive x.
 
@@ -48,7 +48,7 @@ def test_trial_wavefunction_large_positive_x():
 
     assert np.isclose(result, 0, atol=1e-10), "The function did not return a value close to 0 for large positive x."
 
-def test_trial_wavefunction_small_positive_x():
+def test_small_positive_x():
     """
     Test that the trial_wavefunction returns a value close to 1 for small positive x.
 
@@ -76,3 +76,17 @@ def test_uniform_x_between_2_and_3():
     expected_output = np.exp(-alpha * x_uniform)
 
     assert np.allclose(result, expected_output, atol=1e-10), "The function did not return correct exponential values for x uniformly distributed between 2 and 3."
+
+def test_output_array_length():
+    """
+    Test that trial_wavefunction returns an output array with the same length as the input array.
+
+    GIVEN: An array of x values uniformly distributed between 2 and 3.
+    WHEN: The trial_wavefunction function is called with these inputs and a any alpha value.
+    THEN: The function should return an output array with the same length as the input array.
+    """
+    x_uniform = np.random.uniform(low=2.0, high=3.0, size=1000)  
+    alpha = 1.0  
+    result = trial_wavefunction(x_uniform, alpha)
+
+    assert len(result) == len(x_uniform), "The output array does not have the same length as the input array."
